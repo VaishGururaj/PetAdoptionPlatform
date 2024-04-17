bdba0d51fa0990633fb759db6bcb3064474525d1
VaishGururaj <vaishgururaj@gmail.com>
2024-04-17 16:33:48 -0400

    added theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import {Routes, Route, Navigate} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles'; // Import createTheme and ThemeProvider
 import LoginPage from "./components/LoginPage/LoginPage";
 import SignUpPage from "./components/SignUpPage/SignUpPage";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Listpets from "./components/Listpets/Listpets";
 
+// Define your custom theme
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#041464', // Set primary color to #041464
+        },
+    },
+});
 
 function App() {
-
     return (
-        <div className="App">
-            <Routes>
-                <Route path = "/login" element={<LoginPage/>}></Route>
-                <Route path = "/signup" element={<SignUpPage/>}></Route>
-                <Route path="/" element={<Navigate to="/login" />} />
-                <Route path="/dashboard" element={<Dashboard/>}></Route>
-                <Route path = "/listpets" element={<Listpets/>}></Route>
-
-            </Routes>
-        </div>
+        <ThemeProvider theme={theme}> {/* Wrap your entire app with ThemeProvider */}
+            <div className="App">
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<SignUpPage />} />
+                    <Route path="/" element={<Navigate to="/login" />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/listpets" element={<Listpets />} />
+                </Routes>
+            </div>
+        </ThemeProvider>
     );
 }
 
